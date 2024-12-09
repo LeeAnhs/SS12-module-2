ab7c838b991054ab361104f4c7982d69e6f09895
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-12-09 14:24:13 +0700

    Bai2.c

--- Bai2.c
+++ Bai2.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-void InMang () {
-   int arr[100],n;
-   printf("Nhap so mang:");
-   scanf("%d",&n);
-   for(int i=0;i<n;i++){
-       printf("Nhap so phan tu cua mang:");
-       scanf("%d",&arr[i]);
-   }
+void InMang (int arr[100],int n) {
    for(int i=0;i<n;i++){
        printf("%d\n",arr[i]);
    }
-    }
+   }
 int main () {
-    InMang();
+    int arr[100],n;
+    printf("Nhap vao so mang:\n");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        printf("Nhap phan tu trong mang:");
+        scanf("%d",&arr[i]);
+    }
+    InMang(arr,n);
     return 0;
 }
